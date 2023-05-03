62596037618d6a79723cdb7386ee7d6ad80bc5d6
inzilion <inzilion@gmail.com>
2023-05-03 10:42:29 +0900

    채팅방 별로 메시지 전송 구현 완료

--- handlers/game.js
+++ handlers/game.js
@@ -2,9 +2,7 @@ const wss = require('./chat');
 
 const index = (req, res) => {
   res.render("game/multi/index.html", {user : req.session.user});
-  console.dir(wss.clients);
 }
-
 
 module.exports = {
   index,
